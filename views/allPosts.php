7ff632bdc3ad9f31b880ccfe755c45c7cabfe8a5
Bat Paulin <batpaulin@gmail.com>
2018-04-03 17:30:11 +0200

    Mise en place de allpost.php et singlepost.php avec système de commentaires

--- views/allPosts.php
+++ views/allPosts.php
@@ -1,9 +1,18 @@
-<h1>All Posts</h1>
-
-<?php foreach ($allPosts as $post): ?>
-    <div>
-        <h2><?= $post['title']; ?></h2>
-        <small>Posté le <?= $post['post_date']; ?></small>
-        <p><?= $post['content']; ?></p>
+<?php
+use projet4\core\Router;
+?>
+<div class="main-allposts">
+    <h2>Liste des chapitres</h2>
+    <p>Les chapitres sont classés du plus récent au plus ancien.</p>
+    <div class="posts-container">
+        <?php foreach ($allPosts as $post): ?>
+            <div class="post">
+                <div class="post-title"><p><?= $post['title']; ?></p></div>
+                <div class="post-infos">
+                    <div class="post-date"><small>Ajouté le<br> <?= $post['date_fr']; ?></small></div>
+                    <div class="post-read"><a href="<?= Router::getUrl('chapitre') . '?id=' . $post['id'] ?>">Lire le chapitre</a></div>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
-<?php endforeach; ?>
+</div>
--- views/singlePost.php
+++ views/singlePost.php
@@ -1,1 +1,37 @@
-<h1>Test router singlePost.php</h1>
+<?php
+use projet4\core\Router;
+?>
+<div class="main-singlepost">
+    <section class="singlepost-post">
+        <h2><?= $singlePost['title']; ?></h2>
+        <div><?= $singlePost['content'] ?></div>
+    </section>
+    <section class="singlepost-comments">
+        <div class="comment-form">
+            <form action="<?= Router::getUrl('addComment'); ?>?id=<?= $_GET['id'] ?>" method="post">
+                <h3>Ajouter un commentaire</h3>
+                <p>
+                    <label for="comment-author">Votre nom :</label><br>
+                    <input type="text" name="comment-author" id="comment-author" required>
+                </p>
+                <p>
+                    <label for="comment-content">Message :</label><br>
+                    <textarea name="comment-content" id="comment-content" rows="7" cols="30" required></textarea>
+                </p>
+                <p>
+                    <input type="submit" value="Envoyer le commentaire">
+                </p>
+            </form>
+        </div>
+        <div class="comments-container">
+            <h3>Liste des commentaires</h3>
+            <p>Les commentaires sont classés du plus récent au plus ancien.</p>
+            <?php foreach ($commentsByPost as $comment): ?>
+                <div>
+                    <p><strong><?= $comment['author']; ?></strong>, <small>le <?= $comment['date_fr']; ?></small></p>
+                    <p><?= $comment['content']; ?></p>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </section>
+</div>
