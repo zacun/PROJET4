0b1557fa3f8c4ea044e4b1bf0d37ef0d5fa29498
Bat Paulin <batpaulin@gmail.com>
2018-04-12 20:54:11 +0200

    Màj formulaire contact : delete de l'input prénom et ajout de l'input subject

--- views/public/contact.php
+++ views/public/contact.php
@@ -5,30 +5,28 @@ $title = 'Contact';
     <h2>Formulaire de contact</h2>
     <p>
         Pour me contacter, veuillez utiliser le formulaire ci-dessous.<br>
-        Tous les champs sont obligatoires.
+        *Tous les champs sont obligatoires.
     </p>
     <form action="" method="post">
         <fieldset>
             <legend>Vos infos de contact</legend>
             <p>
-                <label for="lastname">Votre nom* :</label><br>
-                <input type="text" name="lastname" id="lastname" placeholder=" Nom" required>
-            </p>
-            <p>
-                <label for="firstname">Votre prénom* :</label><br>
-                <input type="text" name="firstname" id="firstname"placeholder=" Prénom" required>
-            </p>
-            <p>
-                <label for="mail">Votre e-Mail* :</label><br>
+                <label for="lastname">Votre nom* :</label>
+                <input type="text" name="name" id="name" placeholder=" Nom" required>
+                <label for="mail">Votre e-Mail* :</label>
                 <input type="email" name="mail" id="mail" placeholder=" email@exemple.com" required>
             </p>
         </fieldset>
         <fieldset>
             <legend>Votre message</legend>
             <p>
+                <label for="subject">Sujet* :</label><br>
+                <input type="text" name="subject" id="subject" placeholder=" Sujet du message" required>
+            </p>
+            <div>
                 <label for="message">Ecrivez votre message* :</label><br>
                 <textarea id="message" cols="100" rows="15" required></textarea>
-            </p>
+            </div>
             <p>
                 <input type="submit" value="Envoyer le message">
                 <input type="reset" value="Effacer le formulaire">
