cb01bcb2799c48a45231b014e3d4a0b14708a3ac
Bat Paulin <batpaulin@gmail.com>
2018-04-15 23:25:04 +0200

    Correction du système d'authentification.

--- src/model/UsersManager.php
+++ src/model/UsersManager.php
@@ -1,12 +1,15 @@
 <?php
-namespace projet4\src\manager;
+namespace projet4\src\model;
 use projet4\core\Manager;
 
 class UsersManager extends Manager {
 
-    public function getAdmin ($user_name) {
-        $req = $this->prepare('SELECT * FROM users WHERE user = ?', array($user_name), true, true);
-        return $req;
+    public function getAdmin ($user_name, $password) {
+        $admin = $this->prepare('SELECT * FROM users WHERE user = ?', array($user_name), true, true);
+        if (!password_verify($password, $admin['password'])) {
+            return false;
+        }
+        return $admin;
     }
 
 }
